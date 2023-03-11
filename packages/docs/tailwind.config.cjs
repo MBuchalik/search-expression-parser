259bc7507a7f6e7ea5b992ffc374eaef6d8c0a37
MBuchalik <dev@martin-buchalik.de>
2023-03-11 17:14:33 +0100

    Switch to Vite

--- packages/docs/tailwind.config.cjs
+++ packages/docs/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
